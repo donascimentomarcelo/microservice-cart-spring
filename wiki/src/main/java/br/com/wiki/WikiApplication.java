b7934635022eb429a450d936152318a2b3ec56b0
Marcelo Nascimento <marcelo.laravel@gmail.com>
2019-10-30 17:17:01 -0300

    Adding @ComponentScan in Wiki project.

--- wiki/src/main/java/br/com/wiki/WikiApplication.java
+++ wiki/src/main/java/br/com/wiki/WikiApplication.java
@@ -2,8 +2,12 @@ package br.com.wiki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"br.com.core.domains"})
+@ComponentScan({"br.com.core.enums"})
+@ComponentScan({"br.com.core.repositories"})
 public class WikiApplication {
     public static void main( String[] args ) {
     	SpringApplication.run(WikiApplication.class, args);
